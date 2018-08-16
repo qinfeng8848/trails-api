d47b15877fe67205a34c5e00682b0ab098752e02
qinfeng8848 <q884822@gmail.com>
2018-08-16 14:30:45 +0800

    添加路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+namespace :api,:default => {:format =>:json} do
+  namespace :v1 do
+    get "/trains" => "trains#index", :as => :trains
+    get "/trains/:train_number" => "trains#show", :as => :train
+    get "reservations/:booking_code" => "reservations#show", :as => :reservation
+    post "reservations" => "reservations#create", :as => :create_reservations
+    patch "reservations/:booking_code" => "reservations#update", :as => :update_reservations
+    delete "reservations/:booking_code" => "reservations#destroy", :as => :destroy_reservations
+
+  end
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Train.create!(:number => "G533")
+Train.create!(:number => "G519")
+Train.create!(:number => "G503")
+Train.create!(:number => "G587")
