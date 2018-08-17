7f52f07e199d3907a413f7e76cd5ae6d4f5f50ed
qinfeng8848 <q884822@gmail.com>
2018-08-17 18:10:11 +0800

    实现订单的查询更新删除

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,7 +2,19 @@ class Train < ApplicationRecord
   validates_presence_of :number
   has_many :reservations
 
+  # 产生所有位置从 1A~6C
+  # ["1A", "1B", "1C", "2A", "2B", "2C", "3A", "3B", "3C",
+  #  "4A", "4B", "4C", "5A", "5B", "5C", "6A", "6B", "6C"]
+  SEATS = begin
+    (1...6).to_a.map do |series|
+      ["A","B","C"].map do |letter|
+        "#{serise}#{letter}"
+      end
+    end
+  end.flatten
+
   def available_seats
-    ["1A","1B","1C","1D","1F",]
+    # 所有 SEATS 扣掉已经订位的资料
+    return SEATS - self.reservation.pluck(:seat_number)
   end
 end
