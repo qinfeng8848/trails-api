0fb93b7c756e3e482f2b8646b1c5f9e4936e01ae
qinfeng8848 <q884822@gmail.com>
2018-08-22 13:27:39 +0800

    使用api登录注册注销

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::AuthController < ApiController
+  before_action :authenticate_user!, :only => [:logout]
+
+  def signup
+    user = User.new(:email => params[:email],:password => params[:password])
+    if user.save
+      render :json => {:user_id => user.id}
+    else
+      render :json => {:message => "Failed",:errors => user.errors} ,:status => 400
+    end
+  end
+
+  def login
+    if params[:email]&& params[:password]
+      user = User.find_by_email(params[:email])
+    end
+    if user && user.valid_password?(params[:password])
+      render :json => {
+        :message => "OK",
+        :auth_token => user.authentication_token,
+        :user_id => user.id
+      }
+    else
+      render :json => {:message => "邮箱或密码不正确",:errors => user.errors},status => 400
+    end
+  end
+
+  def logout
+    current_user.generate_authentication_token
+    current_user.save!
+    render :json => {:message => "ok"}
+  end
+
+
+end
